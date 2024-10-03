3bbfdbb7aa6f0bb5771a85325880232b4473d97a
victor <odohvictor47@gmail.com>
2024-10-03 06:11:25 +0100

    lil ui changes, tested list product

--- frontend/dayones/src/components/LoginPage.js
+++ frontend/dayones/src/components/LoginPage.js
@@ -5,80 +5,86 @@ import boy from "./boy.png";
 import ethereumicon from "./ethereum-icon.png";
 import mailicon from "./mail-icon.png";
 import lockicon from "./lock-icon.png";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
+  const navigate = useNavigate();
+
+  const handleLogoClick = () => {
+    navigate("/");
+  };
   return (
-    <div className='login-page'>
-      <nav className='login-navbar'>
-        <div className='container'>
-          <div className='logo'>
-            <img src={logo} alt='Dayones Logo' />
-            <span className='logo-text'>dayones</span>
+    <div className="login-page">
+      <nav className="login-navbar">
+        <div className="container">
+          <div className="logo" onClick={handleLogoClick}>
+            <img src={logo} alt="Dayones Logo" />
+            <span className="logo-text">DAYONES</span>
           </div>
-          <span className='login-text'>Login</span>
+          <span className="login-text">Login</span>
         </div>
       </nav>
 
-      <main className='login-main' style={{ height: 'calc(100vh - 66px)' }}>
-        <div className='container login-content'>
-          <div className='login-image'>
-            <img src={boy} alt='Smiling youth' className='rounded-3xl' />
+      <main className="login-main" style={{ height: "calc(100vh - 66px)" }}>
+        <div className="container login-content">
+          <div className="login-image">
+            <img src={boy} alt="Smiling youth" className="rounded-3xl" />
           </div>
-          <div className='login-form  flex flex-col'>
-            <div className='flex-grow'>
-              <h1 className='text-green-800 text-3xl font-extrabold mb-12'>
+          <div className="login-form  flex flex-col">
+            <div className="flex-grow">
+              <h1 className="text-green-800 text-3xl font-extrabold mb-12">
                 Welcome back!
               </h1>
-              <button className='btn-ethereum'>
-                <img src={ethereumicon} alt='Ethereum Icon' />
+              <button className="btn-ethereum">
+                <img src={ethereumicon} alt="Ethereum Icon" />
                 Sign in with Ethereum
               </button>
 
-              <div className='login-divider'>
+              <div className="login-divider">
                 <span>or</span>
               </div>
 
               <form>
-                <div className='form-group'>
-                  <label htmlFor='email'>Email address *</label>
-                  <div className='input-wrapper'>
+                <div className="form-group">
+                  <label htmlFor="email">Email address *</label>
+                  <div className="input-wrapper">
                     <img
                       src={mailicon}
-                      alt='Email Icon'
-                      className='input-icon'
+                      alt="Email Icon"
+                      className="input-icon"
                     />
                     <input
-                      type='email'
-                      id='email'
-                      placeholder='example@gmail.com'
+                      type="email"
+                      id="email"
+                      placeholder="example@gmail.com"
                       required
                     />
                   </div>
                 </div>
 
-                <div className='form-group'>
-                  <label htmlFor='password'>Password </label>
-                  <div className='input-wrapper'>
+                <div className="form-group">
+                  <label htmlFor="password">Password </label>
+                  <div className="input-wrapper">
                     <img
                       src={lockicon}
-                      alt='Password Icon'
-                      className='input-icon'
+                      alt="Password Icon"
+                      className="input-icon"
                     />
                     <input
-                      type='password'
-                      id='password'
-                      placeholder='Enter your password'
+                      type="password"
+                      id="password"
+                      placeholder="Enter your password"
                       required
                     />
                   </div>
                 </div>
 
-                <button type='submit' className='btn-login'>
+                <button type="submit" className="btn-login">
                   Login with password
                 </button>
               </form>
             </div>
-            <footer className='text-center py-4 mt-14 text-gree-800'>
+            <footer className="text-center py-4 mt-14 text-gree-800">
               <p>© Dayones Technologies. All rights reserved.</p>
             </footer>
           </div>
